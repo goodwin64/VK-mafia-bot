dc74a4e2afebf2427e0931b7f77376cce37adc21
Max Donchenko <wereya2@gmail.com>
2016-01-18 15:02:18 +0200

    Work begins.

---
+++ mafia-bot.js
@@ -0,0 +1,13 @@
+;
+/*
+bot for mafia-game: https://mafia.t3h1337.ru/home/#, which framed to VK
+author: Donchenko Max (https://github.com/goodwin64)
+*/
+(function bot() {
+	var money = +($("#co_basic_money")[0].innerHTML.replace(" ", ""));
+		var timerId = setTimeout(function tick() {
+			$("#bank_deposit_amount")[0].value = 4;
+			$("#bank")[0].children[0].children[0].children[0].children[0].children[3].click();
+			timerId = setTimeout(tick, 125);
+		}, 125);
+})();
