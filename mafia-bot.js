c97963a03007e8cfa03cfaefa30d55967a1a5b78
Max Donchenko <wereya2@gmail.com>
2016-01-18 18:54:48 +0200

    first commit in Vim

--- mafia-bot.js
+++ mafia-bot.js
@@ -4,10 +4,11 @@ bot for mafia-game: https://mafia.t3h1337.ru/home/#, which framed to VK
 author: Max Donchenko (https://github.com/goodwin64)
 */
 (function bot() {
-	var money = +($("#co_basic_money")[0].innerHTML.replace(" ", ""));
-		var timerId = setTimeout(function tick() {
+	var money = +($("#co_basic_money")[0].innerHTML.replace(/ /g, ''));
+	for (var i = 0; i < money; i++) {
+		setTimeout(function() {
 			$("#bank_deposit_amount")[0].value = 4;
 			$("#bank")[0].children[0].children[0].children[0].children[0].children[3].click();
-			timerId = setTimeout(tick, 125);
 		}, 125);
+	}
 })();
